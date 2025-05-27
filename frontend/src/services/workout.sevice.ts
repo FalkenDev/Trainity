388ed5ef6ec88d20f5feb0bd20c3f46098a3f408
FalkenDev <falkendev@gmail.com>
2025-05-27 23:29:54 +0200

    Add services to fetch exercises, muscle groups, workouts, and workout sessions

---
+++ frontend/src/services/workout.sevice.ts
@@ -0,0 +1,16 @@
+import { fetchWrapper } from "@/utils/fetchWrapper";
+
+export const fetchAllWorkouts = async () => {
+  const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:8393/v1";
+  try {
+    const response = await fetchWrapper(`${apiUrl}/workouts`);
+    if (!response.ok) {
+      throw new Error("Failed to fetch workouts");
+    }
+    const data = await response.json();
+    return data || [];
+  } catch (error) {
+    console.error("Error fetching workouts:", error);
+    throw error;
+  }
+};
