8f37dae771edda383b0dc855ffea49ed5d82c8bd
FalkenDev <falkendev@gmail.com>
2025-05-27 22:34:23 +0200

    Add vuetify-sonner and pinia-plugin-persistedstate; update App.vue for authentication and modify plugin registration

--- frontend/src/plugins/index.ts
+++ frontend/src/plugins/index.ts
@@ -5,16 +5,15 @@
  */
 
 // Plugins
-import vuetify from './vuetify'
-import pinia from '../stores'
-import router from '../router'
+import vuetify from "./vuetify";
+import pinia from "../stores";
+import router from "../router";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
 // Types
-import type { App } from 'vue'
+import type { App } from "vue";
 
-export function registerPlugins (app: App) {
-  app
-    .use(vuetify)
-    .use(router)
-    .use(pinia)
+export function registerPlugins(app: App) {
+  app.use(vuetify).use(router).use(pinia);
+  pinia.use(piniaPluginPersistedstate);
 }
