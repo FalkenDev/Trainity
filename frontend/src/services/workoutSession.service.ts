30b18694b4ca1d83cc20be24f1c8019ab562ef66
FalkenDev <falkendev@gmail.com>
2025-06-02 23:56:49 +0200

    Add fetchExerciseById and startWorkoutSession functions; refactor error handling in exercise and workout session services

--- frontend/src/services/workoutSession.service.ts
+++ frontend/src/services/workoutSession.service.ts
@@ -1,16 +1,51 @@
-import { fetchWrapper } from '@/utils/fetchWrapper';
+import { fetchWrapper } from "@/utils/fetchWrapper";
 
 export const fetchAllWorkoutSessions = async () => {
-  const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:8393/v1';
+  const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:8393/v1";
   try {
     const response = await fetchWrapper(`${apiUrl}/workoutSessions`);
     if (!response.ok) {
-      throw new Error('Failed to fetch workout sessions');
+      throw new Error("Failed to fetch workout sessions");
     }
     const data = await response.json();
     return data || [];
   } catch (error) {
-    console.error('Error fetching workout sessions:', error);
+    console.error("Error fetching workout sessions:", error);
+    throw error;
+  }
+};
+
+export const startWorkoutSession = async (workoutId: string) => {
+  const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:8393/v1";
+  try {
+    const response = await fetchWrapper(`${apiUrl}/workoutsessions`, {
+      method: "POST",
+      body: JSON.stringify({ workoutId: workoutId }),
+    });
+    if (!response.ok) {
+      throw new Error("Failed to start workout session");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error starting workout session:", error);
+    throw error;
+  }
+};
+
+export const getWorkoutSessionById = async (sessionId: string) => {
+  const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:8393/v1";
+  try {
+    const response = await fetchWrapper(
+      `${apiUrl}/workoutsessions/${sessionId}`
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch workout session");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching workout session:", error);
     throw error;
   }
 };
