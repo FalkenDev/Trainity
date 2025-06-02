85b24a05b9f0d7208069ff1175938a2e4bd347c4
FalkenDev <falkendev@gmail.com>
2025-06-02 23:57:39 +0200

    Implement workout session display with clock and exercise selection functionality (Not yet done)

--- frontend/src/pages/Session.vue
+++ frontend/src/pages/Session.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <BackHeader :show-menu="true" title="Workout">
+    <BackHeader :show-menu="true" title="Workout Session">
       <template #menuAppend>
         <v-list>
           <v-list-item>
@@ -9,7 +9,71 @@
         </v-list>
       </template>
     </BackHeader>
+    <div class="my-3 d-flex justify-space-between align-center bg-grey-darken-4 pa-5">
+      <p class="text-h6">{{ clock }}</p>
+      <v-btn color="primary">Finnish</v-btn>
+    </div>
+    <div class="ma-5">
+        <v-card
+          v-if="!isExerciseStarted"
+          v-for="(exercise, index) in workoutSession?.workout?.exercises"
+          :key="index"
+          class="mb-4 d-flex pa-2 align-center justify-space-between"
+          style="border-radius: 5px"
+        >
+          <div class="d-flex ga-5 align-center">
+            <img class="bg-grey" style="width: 65px; height: 65px">
+            <div class="d-flex flex-column ga-1">
+              <h2 class="text-h6">{{ exercise.exercise?.name }}</h2>
+              <div class="d-flex ga-2">
+                <p class="text-body-2">
+                  {{ exercise.sets }} x {{ exercise.reps }} Reps
+                </p>
+                <p class="text-body-2">
+                  {{ exercise.pauseSeconds }} sec pauses
+                </p>
+                <v-btn color="primary" @click="startExercise(exercise.exerciseId)">Start</v-btn>
+              </div>
+            </div>
+          </div>
+        </v-card>
+        <div v-else>
+          <h1>{{ selectedExercise.name }}</h1>
+        </div>
+      </div>
   </div>
 </template>
 <script lang="ts" setup>
+import { useWorkoutSessionStore } from '@/stores/workoutSession.store';
+import type { WorkoutSession } from '@/interfaces/workoutSession.interface';
+import { fetchExerciseById } from '@/services/exercise.service';
+
+const workoutSessionStore = useWorkoutSessionStore();
+
+const workoutSession = computed<WorkoutSession | null>(() => workoutSessionStore.selectedWorkoutSession);
+
+const clock = computed(() => workoutSessionStore.formattedClock);
+
+const isExerciseStarted = ref(false);
+const selectedExercise = ref(null)
+
+onMounted(() => {
+  if(workoutSessionStore.formattedClock === "00:00") {
+    workoutSessionStore.startClock();
+  }
+});
+
+const startExercise = async(exerciseId: string) => {
+  console.log('Starting exercise with ID:', exerciseId);
+  isExerciseStarted.value = true;
+  const response = await fetchExerciseById(exerciseId);
+  if(response && response._id) {
+    selectedExercise.value = response;
+  } else {
+    console.error('Failed to fetch exercise:', response);
+  }
+
+  console.log('Selected exercise:', selectedExercise.value);
+};
+
 </script>
