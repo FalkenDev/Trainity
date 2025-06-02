316ca0d2ee462b8e22f11ee8480c1357b53f5189
FalkenDev <falkendev@gmail.com>
2025-06-02 23:57:07 +0200

    Enhance workout session store with clock functionality and selected session fetching

--- frontend/src/stores/workoutSession.store.ts
+++ frontend/src/stores/workoutSession.store.ts
@@ -1,14 +1,27 @@
 // stores/authStore.ts
-import { defineStore } from 'pinia';
-import * as workoutSessionService from '@/services/workoutSession.service';
+import { defineStore } from "pinia";
+import * as workoutSessionService from "@/services/workoutSession.service";
 
 export const useWorkoutSessionStore = defineStore(
-  'workoutSessionStore',
+  "workoutSessionStore",
   () => {
     const workoutSessions = ref([]);
+    const selectedWorkoutSession = ref(null);
     const isLoading = ref<boolean>(false);
     const lastFetched = ref<number | null>(null);
     const cacheDuration = 10 * 1000;
+
+    const secondsElapsed = ref(0);
+    let intervalId: ReturnType<typeof setInterval> | null = null;
+    const isRunning = ref(false);
+
+    const formattedClock = computed(() => {
+      const minutes = Math.floor(secondsElapsed.value / 60);
+      const seconds = secondsElapsed.value % 60;
+      const formattedMinutes = String(minutes).padStart(2, "0");
+      const formattedSeconds = String(seconds).padStart(2, "0");
+      return `${formattedMinutes}:${formattedSeconds}`;
+    });
 
     const setWorkoutSessions = async (reload = false) => {
       const now = Date.now();
@@ -27,7 +40,7 @@ export const useWorkoutSessionStore = defineStore(
           await workoutSessionService.fetchAllWorkoutSessions();
         lastFetched.value = now;
       } catch (error) {
-        console.error('Error fetching workout sessions:', error);
+        console.error("Error fetching workout sessions:", error);
       } finally {
         isLoading.value = false;
       }
@@ -35,9 +48,84 @@ export const useWorkoutSessionStore = defineStore(
 
     setWorkoutSessions();
 
-    return { workoutSessions, isLoading, setWorkoutSessions };
+    const fetchSelectedWorkoutSession = async (sessionId: string) => {
+      if (!sessionId) {
+        selectedWorkoutSession.value = null;
+        return;
+      }
+      try {
+        isLoading.value = true;
+        selectedWorkoutSession.value =
+          await workoutSessionService.getWorkoutSessionById(sessionId);
+        if (!selectedWorkoutSession.value) {
+          throw new Error("Workout session not found");
+        }
+      } catch (error) {
+        console.error("Error fetching workout session by ID:", error);
+        selectedWorkoutSession.value = null;
+      } finally {
+        isLoading.value = false;
+      }
+    };
+
+    const startClock = () => {
+      if (intervalId) return;
+
+      intervalId = setInterval(() => {
+        secondsElapsed.value++;
+      }, 1000);
+      isRunning.value = true;
+    };
+
+    const stopClock = () => {
+      if (intervalId) {
+        clearInterval(intervalId);
+        intervalId = null;
+      }
+      isRunning.value = false;
+    };
+
+    const resetClock = () => {
+      stopClock();
+      secondsElapsed.value = 0;
+    };
+
+    watch(
+      isRunning,
+      (newValue) => {
+        if (newValue && intervalId === null) {
+          startClock();
+        } else if (!newValue && intervalId !== null) {
+          stopClock();
+        }
+      },
+      { immediate: true }
+    );
+
+    return {
+      workoutSessions,
+      isLoading,
+      fetchSelectedWorkoutSession,
+      setWorkoutSessions,
+      startClock,
+      stopClock,
+      resetClock,
+      formattedClock,
+      secondsElapsed,
+      isRunning,
+      selectedWorkoutSession,
+    };
   },
   {
-    persist: true,
+    persist: {
+      key: "workoutSessionStore",
+      pick: [
+        "secondsElapsed",
+        "isRunning",
+        "workoutSessions",
+        "selectedWorkoutSession",
+        "lastFetched",
+      ],
+    },
   }
 );
