044a09428326136cb24db3e83b22941d00a348b0
FalkenDev <falkendev@gmail.com>
2025-06-09 23:11:13 +0200

    Implement finish session functionality and improve exercise card layout in Session.vue

--- frontend/src/pages/Session.vue
+++ frontend/src/pages/Session.vue
@@ -9,71 +9,90 @@
         </v-list>
       </template>
     </BackHeader>
-    <div class="my-3 d-flex justify-space-between align-center bg-grey-darken-4 pa-5">
+    <div
+      class="my-3 d-flex justify-space-between align-center bg-grey-darken-4 pa-5"
+    >
       <p class="text-h6">{{ clock }}</p>
-      <v-btn color="primary">Finnish</v-btn>
+      <v-btn color="primary" @click="finnishSession">Finnish</v-btn>
     </div>
     <div class="ma-5">
-        <v-card
-          v-if="!isExerciseStarted"
-          v-for="(exercise, index) in workoutSession?.workout?.exercises"
-          :key="index"
-          class="mb-4 d-flex pa-2 align-center justify-space-between"
-          style="border-radius: 5px"
-        >
-          <div class="d-flex ga-5 align-center">
-            <img class="bg-grey" style="width: 65px; height: 65px">
-            <div class="d-flex flex-column ga-1">
-              <h2 class="text-h6">{{ exercise.exercise?.name }}</h2>
-              <div class="d-flex ga-2">
-                <p class="text-body-2">
-                  {{ exercise.sets }} x {{ exercise.reps }} Reps
-                </p>
-                <p class="text-body-2">
-                  {{ exercise.pauseSeconds }} sec pauses
-                </p>
-                <v-btn color="primary" @click="startExercise(exercise.exerciseId)">Start</v-btn>
-              </div>
+      <v-card
+        v-if="!isExerciseStarted"
+        v-for="(exercise, index) in workoutSession?.workout?.exercises"
+        :key="index"
+        class="mb-4 d-flex pa-2 align-center justify-space-between"
+        style="border-radius: 5px"
+      >
+        <div class="d-flex ga-5 align-center">
+          <img class="bg-grey" style="width: 65px; height: 65px" />
+          <div class="d-flex flex-column ga-1">
+            <h2 class="text-h6">{{ exercise.exercise?.name }}</h2>
+            <div class="d-flex ga-2">
+              <p class="text-body-2">
+                {{ exercise.sets }} x {{ exercise.reps }} Reps
+              </p>
+              <p class="text-body-2">{{ exercise.pauseSeconds }} sec pauses</p>
+              <v-btn color="primary" @click="startExercise(exercise.exerciseId)"
+                >Start</v-btn
+              >
             </div>
           </div>
-        </v-card>
-        <div v-else>
-          <h1>{{ selectedExercise.name }}</h1>
         </div>
+      </v-card>
+      <div v-else>
+        <h1>{{ selectedExercise.name }}</h1>
       </div>
+    </div>
   </div>
 </template>
 <script lang="ts" setup>
-import { useWorkoutSessionStore } from '@/stores/workoutSession.store';
-import type { WorkoutSession } from '@/interfaces/workoutSession.interface';
-import { fetchExerciseById } from '@/services/exercise.service';
+import { useWorkoutSessionStore } from "@/stores/workoutSession.store";
+import type { WorkoutSession } from "@/interfaces/workoutSession.interface";
+import { fetchExerciseById } from "@/services/exercise.service";
+import { finnishWorkoutSession } from "@/services/workoutSession.service";
+import { toast } from "vuetify-sonner";
+import router from "@/router";
 
 const workoutSessionStore = useWorkoutSessionStore();
 
-const workoutSession = computed<WorkoutSession | null>(() => workoutSessionStore.selectedWorkoutSession);
+const workoutSession = computed<WorkoutSession | null>(
+  () => workoutSessionStore.selectedWorkoutSession,
+);
 
 const clock = computed(() => workoutSessionStore.formattedClock);
 
 const isExerciseStarted = ref(false);
-const selectedExercise = ref(null)
+const selectedExercise = ref(null);
 
 onMounted(() => {
-  if(workoutSessionStore.formattedClock === "00:00") {
+  if (workoutSessionStore.formattedClock === "00:00") {
     workoutSessionStore.startClock();
   }
 });
 
-const startExercise = async(exerciseId: string) => {
-  console.log('Starting exercise with ID:', exerciseId);
+const startExercise = async (exerciseId: string) => {
+  console.log("Starting exercise with ID:", exerciseId);
   isExerciseStarted.value = true;
   const response = await fetchExerciseById(exerciseId);
-  if(response && response._id) {
+  if (response && response._id) {
     selectedExercise.value = response;
   } else {
-    console.error('Failed to fetch exercise:', response);
+    console.error("Failed to fetch exercise:", response);
   }
 
-  console.log('Selected exercise:', selectedExercise.value);
+  console.log("Selected exercise:", selectedExercise.value);
 };
 
+const finnishSession = async () => {
+  const response = await finnishWorkoutSession;
+  if (!response) {
+    toast.error("Failed to finish workout session.");
+    return;
+  }
+  workoutSessionStore.stopClock();
+  workoutSessionStore.selectedWorkoutSession = null;
+  workoutSessionStore.resetClock();
+  toast.success("Workout session finished successfully!");
+  router.push("/");
+};
 </script>
