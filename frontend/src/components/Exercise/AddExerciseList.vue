93cf60b5481526f20d5faae045d8074fda0efeb1
Kasper Falk <falkendev@gmail.com>
2025-06-26 17:07:54 +0200

    Enhance AddExerciseList component; add muscle group filtering and improve search functionality

--- frontend/src/components/Exercise/AddExerciseList.vue
+++ frontend/src/components/Exercise/AddExerciseList.vue
@@ -14,15 +14,66 @@
         </v-list>
       </template>
     </BackHeader>
-    <v-text-field
-      v-model="searchQuery"
-      variant="outlined"
-      prepend-inner-icon="mdi-magnify"
-      label="Search exercises"
-      clearable
-      class="mx-2"
-      hide-details
-    />
+    <div class="d-flex flex-row mx-2 ga-5 mb-3">
+      <v-text-field
+        v-model="searchQuery"
+        variant="outlined"
+        prepend-inner-icon="mdi-magnify"
+        label="Search exercises"
+        clearable
+        hide-details
+        density="compact"
+      />
+      <v-badge
+        color="error"
+        :content="selectedMuscleGroups.length"
+        offset-x="0"
+        offset-y="0"
+      >
+        <v-btn
+          height="40"
+          variant="outlined"
+        >
+          Filter
+          <v-menu
+            activator="parent"
+            :close-on-content-click="false"
+          >
+            <v-list
+              v-model:selected="selectedMuscleGroups"
+              select-strategy="classic"
+            >
+              <v-list-item @click="selectedMuscleGroups = []">
+                <v-list-item-title>
+                  <v-icon
+                    class="mr-2"
+                    color="grey-lighten-1"
+                  >
+                    mdi-close
+                  </v-icon>
+                  Reset
+                </v-list-item-title>
+              </v-list-item>
+              <v-divider />
+              <v-list-item
+                v-for="muscleGroup in muscleGroups"
+                :key="muscleGroup._id"
+                :value="muscleGroup._id"
+              >
+                <template #prepend="{ isActive }">
+                  <v-list-item-action start>
+                    <v-checkbox-btn :model-value="isActive" />
+                  </v-list-item-action>
+                </template>
+                <v-list-item-title>
+                  {{ muscleGroup.name }}
+                </v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-btn>
+      </v-badge>
+    </div>
     <v-list>
       <v-list-item
         v-for="exercise in exercises"
@@ -64,6 +115,7 @@
       :selected-exercise="viewExercise"
       :workout-id="props.workoutId"
       :is-view-exercise="true"
+      :is-view-workout-exercise="true"
       @close="isViewExerciseOpen = false"
     />
   </v-dialog>
@@ -82,6 +134,7 @@ import {
   removeExerciseFromWorkout,
 } from "@/services/workout.sevice";
 import { useWorkoutStore } from "@/stores/workout.store";
+import { useMuscleGroupStore } from "@/stores/muscleGroup.store";
 
 // TODO: View exercise details in a modal
 
@@ -95,6 +148,7 @@ const props = defineProps<{
   workoutId: string;
 }>();
 
+const muscleGroupStore = useMuscleGroupStore();
 const workoutStore = useWorkoutStore();
 const searchQuery = ref<string>("");
 const newSelectedExercises = ref<string[]>([]);
@@ -103,20 +157,35 @@ const isLoading = ref<boolean>(false);
 const viewExercise = ref<Exercise | null>(null);
 const isViewExerciseOpen = ref<boolean>(false);
 const isCreateExerciseOpen = ref<boolean>(false);
+const selectedMuscleGroups = ref<string[]>([]);
 
 const openViewExercise = (exercise: Exercise) => {
   viewExercise.value = exercise;
   isViewExerciseOpen.value = true;
 };
 
+const muscleGroups = computed(() => {
+  return muscleGroupStore.muscleGroups.map((group) => ({
+    name: group.name,
+    _id: group._id,
+  }));
+});
+
 const exercises = computed<Exercise[]>(() =>
   exerciseStore.exercises.filter((exercise: Exercise) => {
-    return (
+    const matchesSearch =
       exercise.name.toLowerCase().includes(searchQuery.value.toLowerCase()) ||
-      exercise.description
+      (exercise.description ?? "")
         .toLowerCase()
-        .includes(searchQuery.value.toLowerCase())
-    );
+        .includes(searchQuery.value.toLowerCase());
+
+    const matchesMuscleGroup =
+      selectedMuscleGroups.value.length === 0 ||
+      exercise.muscleGroups?.some((mg: string) =>
+        selectedMuscleGroups.value.includes(mg)
+      );
+
+    return matchesSearch && matchesMuscleGroup;
   }),
 );
 
